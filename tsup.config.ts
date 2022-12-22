5f3a379c942f5560fd228eee144bdf377e5bd65f
Jaron <60853956+JaronZ@users.noreply.github.com>
2022-12-22 18:06:27 +0000

    fix: add js support

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import type { Options } from "tsup";
 export const tsup: Options = {
 	clean: true,
 	dts: false,
-	entryPoints: ["src/**/*.ts", "!src/**/*.d.ts", "!src/lib/**/*"],
+	entryPoints: ["src/**/*.{js,ts}", "!src/**/*.d.ts", "!src/lib/**/*"],
 	format: ["cjs"],
 	minify: true,
 	skipNodeModulesBundle: true,
